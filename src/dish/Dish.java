fb674f463161f58791c76842a5254ccf0cc25e2e
KylinDC <inlovingdays@gmail.com>
2019-02-03 15:48:15 +0800

    add the class of dish

---
+++ src/dish/Dish.java
@@ -0,0 +1,37 @@
+package dish;
+
+public class Dish {
+    private String id;
+    private double price;
+    private String name;
+
+    public Dish(String id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
